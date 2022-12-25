5f8d4dbcf6ba4afa198004153c9fe42531877ba1
Natalia Semenova <natalia.semyonova@gmail.com>
2022-12-25 12:54:32 +0300

    Project created

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+
+        int pricePerTicket = 125; // Цена за билет
+        int costPerMile = 20; // Количество рублей для одной бонусной мили
+        int countOfMiles; // Количество бонусных миль
+
+        countOfMiles = pricePerTicket/costPerMile; // Рассчитывает количество бонусных миль
+
+        System.out.println(countOfMiles + " миллилитров воды"); //Вывод результата на экран
+    }
+}
